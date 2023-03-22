b1a9b26f5b625eeeb75da5e699992fce1ea11565
dawid-polak <dawidow.polak@gmail.com>
2023-03-22 10:48:28 +0100

    add section mountain in about

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,7 +13,10 @@ module.exports = {
       'color-A9DCEC': '#A9DCEC',
       'color-8002E0': '#8002E0',
       'color-9AA09E': '#9AA09E',
-      'color-FFFFFF': '#FFFFFF'
+      'color-FFFFFF': '#FFFFFF',
+      'color-ff5708': '#ff5708',
+      'color-47270b': '#47270b',
+      'color-1c494c': '#1c494c'
     },
   },
   plugins: [],
